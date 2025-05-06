19aebc0f01b5b70b9411bcdffbc87753c9b17eb2
Aahan Salecha <salecha.aahan@gmail.com>
2025-05-06 15:26:51 +0530

    Add password validation
    Added password validation for
    - Password must be at least 8 characters long
    - Password must contain at least one digit
    - Password must contain at least one letter
    - Password must contain at least one special character

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -3,7 +3,7 @@ from .. import schemas
 from .. import models
 from .. import utils
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 router = APIRouter(
@@ -12,7 +12,14 @@ router = APIRouter(
 )
 
 @router.post("/", response_model=schemas.UserResponse, status_code=201)
-async def create_user(user: schemas.UserCreate, db: Session = Depends(getDB)):  
+async def create_user(user: schemas.UserCreate, db: Session = Depends(getDB)):    
+    validation = utils.validate_password(user.password)
+    if validation != True:
+        raise HTTPException(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, 
+            detail=validation
+        )
+
     user.password = utils.hash(user.password)  
     db_user = models.User(**user.model_dump())
 
--- backend/app/utils.py
+++ backend/app/utils.py
@@ -1,6 +1,24 @@
+from typing import Literal
 from passlib.context import CryptContext
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 def hash(password: str):
     return password_context.hash(password)
+
+def validate_password(password: str) -> Literal[
+    True, 
+    "Password must be at least 8 characters long", 
+    "Password must contain at least one digit", 
+    "Password must contain at least one letter", 
+    "Password must contain at least one special character"
+]:
+    if len(password) < 8:
+        return "Password must be at least 8 characters long"
+    if not any(char.isdigit() for char in password):
+        return "Password must contain at least one digit"
+    if not any(char.isalpha() for char in password):
+        return "Password must contain at least one letter"
+    if not any(char in "!@#$%^&*()-_+=<>?{}[]|~`" for char in password):
+        return "Password must contain at least one special character"
+    return True
